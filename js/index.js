4f87651a0044b402f899232f2ba203d599654480
williamprussell <98131974+williamprussell@users.noreply.github.com>
2022-07-30 18:22:25 +1000

    Further changes 30.7.22

--- js/index.js
+++ js/index.js
@@ -4,3 +4,10 @@ $('.work').click(function (e) {
         scrollTop: $('.case-study-set-1').offset().top
     }, 1000);
 });
+
+$('.about').click(function (e) {
+    console.log("hello");
+    $('html, body').animate({
+        scrollTop: $('#about-me').offset().top
+    }, 1500);
+});
